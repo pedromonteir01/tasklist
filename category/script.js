31898f040503fa06aa86398cd85df5aab2ce6679
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-08-17 10:17:08 -0300

    feat: add product class

--- category/script.js
+++ category/script.js
@@ -5,3 +5,12 @@ class Category {
         this.products = [];
     }
 }
+
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
