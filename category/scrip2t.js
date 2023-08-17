dc294ca68fe4ec0b8bd7e36c44f1234309eb5870
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-08-17 11:11:57 -0300

    feat: create products and product

---
+++ category/scrip2t.js
@@ -0,0 +1,53 @@
+class Category {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.products = [];
+    }
+}
+
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+
+    addCategory(name) {
+        const id = this.nextCategoryId++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+}
+
+class ProductsServices {
+    constructor() {
+        this.products = [];
+        this.nextProductId = 0;
+    }
+
+    addProduct(name, price, category) {
+        const id = this.nextProductId++;
+        const product = new Product(id, name, price, category);
+        category.products.push(product);
+        this.products.push(product);
+    }
+}
+
+const categoryList = new CategoryService();
+
+function createCategory() {
+    const categoryName = "Doce";
+
+    categoryList.addCategory(categoryName);
+
+    console.log(categoryList.categories);
+}
